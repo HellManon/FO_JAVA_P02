31bb7e9a7df9ce553f27bd3fc26df1f8eec6817a
HellManon <manon.hell@outlook.fr>
2021-10-28 11:50:20 +0200

    débugage AnalyticsCounter, correction de la boucle de comptage, suppression des variables inutiles, supressiondes commentaires inutiles, réédition des commentaires, redéfinition du chemin pour que le fichier result.out se génère.

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,36 +5,36 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt"));
-		String line = reader.readLine();
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+	public static void main(String args[]) throws Exception {
+
+		
+		BufferedReader reader = new BufferedReader(new FileReader("C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt"));
+		String line = reader.readLine();
+		// loop to count symptoms
+		int i = 0;
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
+			i++;
+
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+				headacheCount++;
+			} else if (line.equals("rash")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.equals("dialated pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		System.out.println("number of headaches: " + headacheCount);
+		System.out.println("number of rash: " + rashCount);
+		System.out.println("number of pupils: " + pupilCount);
+
+		// generate file result.out
+		FileWriter writer = new FileWriter("C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\\\com\\hemebiotech\\\\analytics\\result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
