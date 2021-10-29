f2bf237781ac3370622a487b1affaf0143415c6a
HellManon <manon.hell@outlook.fr>
2021-10-29 17:40:47 +0200

    implémentation de la class ReadSymptomDataFromFile pour lire le fichier, ajout de start() dans la class AnalyticsCounter, déclaration d'une map pour compter les symptomes, création d'une boucle pour compter et implementer la map(à finir)

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,53 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 
-	public static void main(String args[]) throws Exception {
-
+	public void start() throws Exception{
 		
-		BufferedReader reader = new BufferedReader(new FileReader("C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt"));
-		String line = reader.readLine();
-		// loop to count symptoms
-		int i = 0;
-		while (line != null) {
-			i++;
-
-			if (line.equals("headache")) {
-				headacheCount++;
-			} else if (line.equals("rash")) {
-				rashCount++;
-			} else if (line.equals("dialated pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();
-		}
-		System.out.println("number of headaches: " + headacheCount);
-		System.out.println("number of rash: " + rashCount);
-		System.out.println("number of pupils: " + pupilCount);
-
+		String filepath = "C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt";
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile(filepath);
+		List<String> symptoms = symptomReader.GetSymptoms();
+		// déclarer la map treemap
+		 TreeMap<String, Integer> map = new TreeMap<>();
+		
+		//faire la boucle de comptage
+		 for (String symptom: symptoms) {
+			
+			 if(!map.containsKey(symptom)) {
+				 map.merge(symptom,  1, Integer::sum);
+				 //System.out.println(map);
+			 }
+			 else {
+				 map.put(symptom, +1);
+				 System.out.println(map);
+			 }
+		 }
+				 
+		 //si symptom = clé map
+		 	//ajouter symptom en clé map
+		 	//ajouter à la valeur valeur
+		 		//OK
+		//sinon
+		 	// value map ++
+		
 		// generate file result.out
 		FileWriter writer = new FileWriter("C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\\\com\\hemebiotech\\\\analytics\\result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}
+	
+	public static void main(String args[]) throws Exception {
+		AnalyticsCounter analyticsCouter = new AnalyticsCounter();
+		analyticsCouter.start();
+		
 	}
 }
