c0f34f4e89a6e4f4b83b56735a51275df11e7f71
HellManon <manon.hell@outlook.fr>
2021-11-25 15:54:31 +0100

    refactoring du code en sous-méthode

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,58 +2,62 @@ package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
 /**
- * Everything that allows the program to be run The part that allows you to
- * count the occurrences and sort them in ascending order The part that allows
- * you to write the result to a file
+ * this class allows to count the data of a file and to write the result in a
+ * file
  * 
  */
 
 public class AnalyticsCounter {
 
-	/**
-	 * 
-	 * @param start count the occurences write the result to a file
-	 */
-
-	public void start() throws Exception {
+	public void start() {
 
 		String filepath = "C:\\Formation openclassrooms\\Développeur d'applications JAVA\\FO_JAVA_P02\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt";
 		ISymptomReader symptomReader = new ReadSymptomDataFromFile(filepath);
 		List<String> symptoms = symptomReader.GetSymptoms();
 
-		// declaration de la map
-		TreeMap<String, Integer> map = new TreeMap<>();
-		try {
-			// boucle de comptage
-			for (String symptom : symptoms) {
+		Map<String, Integer> countSymptoms = countSymptoms(symptoms);
+		writeResult(countSymptoms);
+	}
 
-				if (!map.containsKey(symptom)) {
-					map.merge(symptom, 1, Integer::sum);
-				} else {
-					int count = map.containsKey(symptom) ? map.get(symptom) : 0;
-					map.put(symptom, count + 1);
-				}
+	/**
+	 * write the result to a file result.out
+	 * 
+	 * @param countSymptoms the map that contain the number of symptoms
+	 * 
+	 */
+	private void writeResult(Map<String, Integer> countSymptoms) {
+		try (FileWriter writer = new FileWriter("result.out")) {
+			for (Entry<String, Integer> mapentry : countSymptoms.entrySet()) {
+				writer.write(mapentry.getKey() + "=" + mapentry.getValue() + "\n");
 			}
-			System.out.println(map);
-		} catch (Exception e) {
-			System.out.println("Erreur d'execution de la boucle de comptage");
-		}
-
-		// generation du fichier result.out
-		try {
-			FileWriter writer = new FileWriter("result.out");
-			for (Entry<String, Integer> mapentry : map.entrySet()) {
-				System.out.println("clé: " + mapentry.getKey() + " | valeur: " + mapentry.getValue());
-				writer.write(mapentry.getKey() + " " + mapentry.getValue() + "\n");
-			}
-			writer.close();
 		} catch (Exception e) {
 			System.out.println("erreur lors de la creation du fichier result");
 		}
+	}
+
+	/**
+	 * count the number of symptoms of the list
+	 * 
+	 * @param symptoms the list of symptoms
+	 * @return a map that contain the symptoms and their number
+	 */
+	private Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> countSymptoms = new TreeMap<>();
+
+		for (String symptom : symptoms) {
+			if (!countSymptoms.containsKey(symptom)) {
+				countSymptoms.put(symptom, 1);
+			} else {
+				int count = countSymptoms.get(symptom);
+				countSymptoms.put(symptom, count + 1);
+			}
+		}
+		return countSymptoms;
 	}
 
 	public static void main(String args[]) throws Exception {
